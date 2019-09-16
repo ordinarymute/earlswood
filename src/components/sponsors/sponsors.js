06d7812056f3e47fceef98e630a64cbb94fef5ac
OrdinaryMute <ordinarymute@gmail.com>
2019-09-15 20:04:49 -0500

    test push

--- src/components/sponsors/sponsors.js
+++ src/components/sponsors/sponsors.js
@@ -4,6 +4,8 @@ const Sponsors = () => {
   return (
     <div className="inner-container">
       <h1>Sponsors</h1>
+      <h2>I am still testing!</h2>
+      <h3>Bob!</h3>
       <div className="sponsors">
         <a href=""><img src="" alt=""/></a>
       </div>
