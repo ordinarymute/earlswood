bf982903eb3573308c85daa152d7830716b340a1
OrdinaryMute <ordinarymute@gmail.com>
2019-09-15 20:08:32 -0500

    remove test tags

--- src/components/sponsors/sponsors.js
+++ src/components/sponsors/sponsors.js
@@ -4,8 +4,6 @@ const Sponsors = () => {
   return (
     <div className="inner-container">
       <h1>Sponsors</h1>
-      <h2>I am still testing!</h2>
-      <h3>Bob!</h3>
       <div className="sponsors">
         <a href=""><img src="" alt=""/></a>
       </div>
