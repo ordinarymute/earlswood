d2060d02a2b65e098d36cccd71599be802d034e4
OrdinaryMute <ordinarymute@gmail.com>
2019-09-15 20:11:06 -0500

    One last test

--- src/components/sponsors/sponsors.js
+++ src/components/sponsors/sponsors.js
@@ -4,6 +4,7 @@ const Sponsors = () => {
   return (
     <div className="inner-container">
       <h1>Sponsors</h1>
+      <h1>Test 1 2 3!</h1>
       <div className="sponsors">
         <a href=""><img src="" alt=""/></a>
       </div>
